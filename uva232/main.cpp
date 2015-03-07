4f9fe17eeed6a2cc205021f4d9e737bcc201b8ed
huangwenjun <884719803@qq.com>
2015-03-07 12:07:39 +0800

    uva232-1

--- uva232/main.cpp
+++ uva232/main.cpp
@@ -10,7 +10,7 @@ int main()
     if(n)
     {
         cin>>m;
-        char k[n-1][m-1];
+        char puzzle[n-1][m-1];
         string s;
         vector<char> num;
         vector<int> low,col;
@@ -19,15 +19,15 @@ int main()
             cin>>s;
             for(int j=0;j<m;j++)
             {
-                k[i][j]=s[j];
+                puzzle[i][j]=s[j];
             };
         };
        for(int i=0;i<n;i++)
        {
         for(int j=0;j<m;j++)
         {
-            if((i==0)||(j==0)||(k[i][j-1]='*')||(k[i-1][j]))
-            {num.push_back(k[i][j]);low.push_back(i);col.push_back(j);}
+            if((i==0)||(j==0)||(puzzle[i][j-1]='*')||(puzzle[i-1][j]))
+            {num.push_back(puzzle[i][j]);low.push_back(i);col.push_back(j);}
         }
        }
        int countlow=1,count=1;
@@ -38,13 +38,51 @@ int main()
            for(int j=0;j<m;j++)
            {
                int k=0;
-               if((k[i][j]==num[k])&&(i==low[k])&&(j==col[k]))
+               if((puzzle[i][j]==num[k])&&(i==low[k])&&(j==col[k]))
                {
                    cout<<"  "<<k<<'.';
-
+                   int low1=1;
+                   for(int x=j+low1;x<m;x++)
+                   {
+                       if((puzzle[i][x]==num[x])&&(i==low[x])&&(x==col[x]))
+                       {
+                           cout<<puzzle[i][x];
+                       }
+                       else
+                       {
+                           break;
+                           cout<<endl;
+                       }
+                   }
+               }
+           }
+       };
+       cout<<"Down"<<endl;
+       for(int i=0;i<n;i++)
+       {
+           for(int j=0;j<m;j++)
+           {
+               int k=0;
+               if((puzzle[i][j]==num[k])&&(i==low[k])&&(j==col[k]))
+               {
+                   cout<<"  "<<k<<'.';
+                   int col1=1;
+                   for(int x=i+col1;x<n;x++)
+                   {
+                       if((puzzle[x][j]==num[x])&&(x==low[x])&&(j==col[x]))
+                       {
+                           cout<<puzzle[x][j];
+                       }
+                       else
+                       {
+                           break;
+                           cout<<endl;
+                       }
+                   }
                }
            }
        }
+    }
     return 0;
 }
 
